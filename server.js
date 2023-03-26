5c121f8477b927dff51850a29eb0a6c57d15dd83
Yusuf <yumustyologt@example.com>
2023-03-26 15:33:31 +0100

    updates

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.post("/places/tourist_attraction", async (req, res) => {
           international_phone_number:
             reviewsData?.result?.international_phone_number,
           user_reviews: reviewsData?.result?.reviews,
+          website: reviewsData?.result?.website,
+          vicinity: reviewsData?.result?.vicinity,
           attraction_images: reviewsData?.result?.photos?.map(
             (photo, i) =>
               `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photo.photo_reference}&key=${key}`
@@ -81,7 +83,7 @@ app.post("/places/type_search", async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/place/textsearch/json
 ?location=${lat}%2C${lng}
 &language=${language}
-&type=${type}
+&types=${type}
 &radius=${radius}
 &key=${key}`;
   try {
@@ -112,6 +114,8 @@ app.post("/places/type_search", async (req, res) => {
           international_phone_number:
             reviewsData?.result?.international_phone_number,
           user_reviews: reviewsData?.result?.reviews,
+          website: reviewsData?.result?.website,
+          vicinity: reviewsData?.result?.vicinity,
           attraction_images: reviewsData?.result?.photos?.map(
             (photo, i) =>
               `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photo.photo_reference}&key=${key}`
@@ -128,7 +132,86 @@ app.post("/places/type_search", async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal server error" });
+    res
+      .status(500)
+      .json({ error: "Internal server error", message: error.message });
+  }
+});
+
+// keyword search api
+
+app.post("/places/keyword_search", async (req, res) => {
+  const {
+    language = "en",
+    // radius = 2000,
+    radius = 3000,
+    // lng = -73.58781,
+    // lat = 45.50884,
+    lat =  6.592922,
+    lng = 3.339059,
+    type = "restaurant",
+    keyword = "imperial",
+  } = req.body;
+
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json
+?input=${keyword}
+&location=${lat}%2C${lng}
+&radius=${radius}
+&types=${type}
+&key=${key}
+&language=${language}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const structuredData = await Promise.all(
+      data?.predictions?.map(async (attraction, i) => {
+        const fetchReviews = await fetch(
+          `https://maps.googleapis.com/maps/api/place/details/json?placeid=${attraction.place_id}&key=${key}`
+        );
+        const reviewsData = await fetchReviews.json();
+
+        return {
+          editorial_summary: reviewsData?.result?.editorial_summary,
+          weekday_text: reviewsData?.result?.current_opening_hours,
+          business_status: reviewsData?.result?.business_status,
+          attraction_address: reviewsData?.result?.formatted_address,
+          terms: attraction?.terms,
+          attraction_name: reviewsData?.result?.name,
+          description: attraction?.description,
+          ratings: reviewsData?.result?.rating,
+          reference: attraction?.reference,
+          attraction_types: reviewsData?.result?.types,
+          user_ratings_total: reviewsData?.result?.user_ratings_total,
+          geometry: reviewsData?.result?.geometry,
+          international_phone_number:
+            reviewsData?.result?.international_phone_number,
+          formatted_phone_number:
+            reviewsData?.result?.formatted_phone_number,
+          map_url: reviewsData?.result?.url,
+          attraction_images: reviewsData?.result?.photos?.map(
+            (photo, i) =>
+              `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photo.photo_reference}&key=${key}`
+          ),
+          website: reviewsData?.result?.website,
+          vicinity: reviewsData?.result?.vicinity,
+          user_reviews: reviewsData?.result?.reviews,
+        };
+      })
+    );
+
+
+    res.json({
+      data: {
+        places: structuredData,
+        success: true,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", message: error.message });
   }
 });
 
